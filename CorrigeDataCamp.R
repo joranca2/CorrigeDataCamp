d6b988c6533718db074e15d1389369ba720c0522
joranca2 <joranca2@alumni.uv.es>
2026-04-09 13:35:51 +0200

    Creamos el dataframe, despues los combinamos con el que ya teniamos y creamos los ficheros usando las librerias necesarias.

--- CorrigeDataCamp.R
+++ CorrigeDataCamp.R
@@ -34,7 +34,9 @@ for(i in 1:length(ficheros_puntos)){
   
   carpeta_alumno <- partes[length(partes)-1]
   
-  texto_alumno <- strsplit(carpeta_alumno, split = "_", fixed = TRUE)[[1]][1]
+  partes_carpeta <- strsplit(carpeta_alumno, split = "_", fixed = TRUE)[[1]]
+  
+  texto_alumno <- partes_carpeta[1]
   
   apellido_i <- strsplit(texto_alumno, split = ",", fixed = TRUE)[[1]][1]
   apellidos <- c(apellidos, apellido_i)
@@ -83,3 +85,37 @@ TieneJustificante <- apellidos %in% apellidos_justificante
 
 all(TieneJustificante)
 apellidos[!TieneJustificante]
+
+#Convierto puntos a cadena de texto:
+
+Puntos <- as.character(puntos)
+
+#Creamos el dataframe con sus respectivas columnnas:
+
+evaluados_df <- data.frame(apellidos, puntos, NomFile, Puntos)
+
+evaluados_df <- evaluados_df[order(evaluados_df$apellidos), ]
+
+evaluados_df
+
+#La carga y creacion de ficheros:
+
+install.packages("readxl")
+library(readxl)
+
+alumnos_df <- read_excel("AlumnosTD25_26.xlsx")
+
+alumnos_df$apellidos <- alumnos_df[["Apellido(s)"]] #Uno por apellidos porque no se como 
+                                                     #hacer con ID, aunque si hay apellidos iguales
+                                                      #y tal puede fallar.
+
+AlumnosNotas <- merge(alumnos_df, evaluados_df, by = "apellidos", all.x = TRUE)
+
+install.packages("openxlsx")
+library(openxlsx)
+
+write.xlsx(evaluados_df, "NotasRIntermedio.xlsx", rowNames = FALSE)
+write.xlsx(AlumnosNotas, "AlumnosNotas.xlsx", rowNames = FALSE)
+
+writeLines("https://github.com/joranca2/CorrigeDataCamp.git", "EnlaceRepo.txt")
+
